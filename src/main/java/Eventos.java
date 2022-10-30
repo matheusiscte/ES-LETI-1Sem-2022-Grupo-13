c37e696f9dc98ea34b511ce89f22a86c529ed2b3
matheusiscte <mpclq@iscte-iul.pt>
2022-10-30 19:16:47 +0000

    novo commit

---
+++ src/main/java/Eventos.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Eventos {
+		
+		private ArrayList<Event> listaEventos;
+
+		public ArrayList<Event> getListaEventos() {
+			return listaEventos;
+		}
+
+		public void setListaEventos(ArrayList<Event> listaEventos) {
+			this.listaEventos = listaEventos;
+		}
+		
+	}
+	
